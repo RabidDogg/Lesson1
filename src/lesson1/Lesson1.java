c1d21f0b21a837f88992701473172254264d416b
RabidDogg <platosh2012@yandex.ru>
2015-01-19 18:03:06 +0300

    Program1

---
+++ src/lesson1/Lesson1.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+import javax.swing.JFrame;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       JFrame window = new JFrame();
+       window.setTitle("Hello Java");
+       window.setBounds(10, 20, 500, 600);
+       window.setVisible(true);
+	}
+
+}
